daf6380b2b9d2fc2d3224ea2440c833f77fe5161
Kuba7331 <kuba@kamionka.eu>
2023-02-05 18:59:02 +0100

    Algorithm which checks if point belongs to line.

---
+++ pointOnLine.py
@@ -0,0 +1,19 @@
+import random
+
+point1 = [1,2]
+point2 = [2,4]
+
+point = [3,6]
+
+def ifOnLine(point1,point2,point):
+    a = (point2[1] - point1[1]) / (point2[0] - point1[0])
+    b = point2[1] - a*point2[0]
+    if a*point[0] + b == point[1]:
+        return True
+    return False
+
+if ifOnLine(point1,point2,point) == True:
+    print("Punkt nalezy do prostej.")
+else:
+    print("Punkt nie należy do prostej.")
+
