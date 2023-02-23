180f3aa9919a06c62075115f9de10dd9a0f9d9bb
Kuba7331 <kuba@kamionka.eu>
2023-02-23 13:04:01 +0100

    Comment update

--- fibbonacciSequence.py
+++ fibbonacciSequence.py
@@ -1,11 +1,25 @@
 
+# Ciag Fibbonacciego - algorytm wyznaczajacy ciag liczb, skladajacy sie z sumy dwoch poprzednich liczb (1, 1, 2, 3, 5, 8...)
+
+# Tworzymy dwuelementowa tablice, skladajaca sie z dwoch poczatkowych liczb z ciagu Fibbonacciego.
+
 tabFib = [1, 1]
+
+# Wpisujemy dlugosc ciagu
 
 fibNumber = int(input())
 
+# t
+
 def FibonacciSeq(number):
+
+    # Sprawdzamy czy podana przez nas liczba jest mniejsza, lub rowna dwa, jesli jest, wypisujemy tylko 1 zgodnie z definicja ciagu.
+
     if number <= 2:
         return 1
+
+    # Dla liczb wiekszych od 2, sumujemy sumy dwoch poprzednich liczb i owy element, dodajemy do listy, ten proces powtarza sie az do momentu koncowego indeksu podanego w zakresie petli.
+
     else:
         for i in range(2, number):
             tabFib.append(tabFib[i-1] + tabFib[i-2])
