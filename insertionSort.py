193d4f0db7c84184baf00c2b5c65857b17e52eb4
Kuba7331 <kuba@kamionka.eu>
2023-02-25 15:00:16 +0100

    Comment update

--- insertionSort.py
+++ insertionSort.py
@@ -3,7 +3,7 @@ import time
 
 tabel = []
 
-tabelLength = 5000
+tabelLength = 10
 
 for i in range(0, tabelLength - 1):
     tabel.append(random.randint(0,100))
@@ -13,15 +13,31 @@ print("Tabel before insertion sort:")
 for j in tabel:
     print(j, end=" ")
 
-def insertionSort(listOfNumbers):
-    for i in range(1, tabelLength - 1):
-        j = i - 1
-        comp = listOfNumbers[i]
-        while j > -1 and listOfNumbers[j] > comp:
-            listOfNumbers[j+1] = listOfNumbers[j]
+# Insertion sort - prosty algorytm sortowania, ktory nie wymaga dodatkowej pamieci komputera.
+
+def insertionSort(tab):
+
+    # Zaczyna sie iteracje petli przechodzaca przez cala tablice, od drugiego elementu tablicy
+
+    for i in range(1, len(tab)):
+
+        # Przyrownujemy wartosc zmiennej j do zmiennej i
+
+        j = i
+
+        # Tworzymy petle, ktora dziala do momentu, az element tab[j-1] napotka element wiekszy od siebie.
+
+        while j > 0 and tab[j - 1] > tab[j]:
+
+            # W wypadku, gdy liczba porownywana z lewej strony jest wieksza od prawej, dochodzi do zamiany elementow listy miejscami.
+
+            tab[j], tab[j - 1] = tab[j - 1], tab[j]
+
+            # Przesuwamy mniejszy element w lewa strone tablicy, do momentu, az napotka na mniejszy element od siebie.
+            # Dziala to poprzez zmniejszanie indeksu tablicy o 1.
+
             j -= 1
-        listOfNumbers[j+1] = comp
-    return listOfNumbers
+    return tab
 
 print("\nTabel after insertion sort:")
 
