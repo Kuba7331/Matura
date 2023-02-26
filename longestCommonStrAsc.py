57d40473cc1981a1f6d4fa3fb02f9c51ee067454
Kuba7331 <kuba@kamionka.eu>
2023-02-26 20:42:12 +0100

    Comment update

--- longestCommonStrAsc.py
+++ longestCommonStrAsc.py
@@ -2,6 +2,8 @@ import  random
 
 tab = []
 
+# Generujemy 10-elementowa tablice, z losowymi liczbami z zakresu 1-10.
+
 for i in range(0, 10):
     tab.append(random.randint(0,10))
 
@@ -10,19 +12,19 @@ print("Tabel:")
 for j in tab:
     print(j, end=" ")
 
-def longestAscSubStr(tabel):
-    tabLength = len(tabel)
-    max = -1000
-    pointer = 0
-    for i in range(len(tabel)):
-        if pointer < i:
-            pointer = i
-        while pointer < tabLength - 1 and tabel[pointer] < tabel[pointer + 1]:
-            pointer += 1
-        varOne = pointer - i + 1
-        if varOne > max:
-            max = varOne
-        pointer = 0
-    return max
+# Funkcja sprawdza najdluzszy rosnacy ciag liczb w tablicy.
+
+def longestAscSubStr(arr):
+    n = len(arr)
+    max_len = 1  # maksymalna długość podciągu niemalejącego
+    cur_len = 1  # aktualna długość podciągu niemalejącego
+    for i in range(1, n):   # petla przechodzi przez 2-gi element tablicy, do jej konca.
+        if arr[i] > arr[i-1]: # porownujemy liczby miedzy soba, jesli liczba, po prawej stronie jest wieksza od tej po lewej, dodajemy 1 do najdluzszego podciagu rosnacych liczb w tablicy.
+            cur_len += 1
+        else: # sprawdzamy aktualny podciag, jesli jest wiekszy od maksymalnego, to maksymalny przyjmuje wartosc aktualnego, w innym wypadku, aktualna dl. ciagu jest rowna 1.
+            max_len = max(max_len, cur_len)
+            cur_len = 1
+    max_len = max(max_len, cur_len)  # sprawdzamy ostatni podciąg
+    return max_len
 
 print("\nLongest ascending number string in tabel has a length of:", longestAscSubStr(tab),"numbers")
