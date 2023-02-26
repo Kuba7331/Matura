3ba4616bc79fa1fd1369556ff6d0c6e0f1ade712
Kuba7331 <kuba@kamionka.eu>
2023-02-26 20:28:59 +0100

    Comment update

--- longerString.py
+++ longerString.py
@@ -1,11 +1,22 @@
 string1 = input()
 string2 = input()
 
+# Algorytm sprawdza ktory wyraz/ciag znakow jest dluzszy, po liczbie jego znakow. (Funkcja len())
+
 def whichIsLonger(stringOne, stringTwo):
+
+    # W wypadku, gdy dlugosc znaku pierwszego jest wieksza od drugiego, to analogicznie, pierwszy jest dluzszy.
+
     if len(stringOne) > len(stringTwo):
         return print(stringOne, "is longer a string.")
+
+    # W przeciwnym razie, drugi jest dłuzszy.
+
     elif len(stringTwo) > len(stringOne):
         return print(stringTwo, "is longer a string.")
+
+    # W wypadku, gdy oba stringi maja ta sama dlugosc, to sa sobie rowne.
+
     elif len(stringOne) == len(stringTwo):
         return print("Both strings have an equal length.")
 
