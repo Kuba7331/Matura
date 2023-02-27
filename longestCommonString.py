0cab5f859906bd86ec2f9540eb5bf316d81afcaf
Kuba7331 <kuba@kamionka.eu>
2023-02-27 20:10:25 +0100

    Comment update

--- longestCommonString.py
+++ longestCommonString.py
@@ -1,3 +1,6 @@
+
+# Algorytm sprawdza nadluzszy wspolny podciag z obu wyrazow.
+
 stringOne = input()
 stringTwo = input()
 
@@ -6,8 +9,12 @@ stringLengthTwo = len(stringTwo)
 
 tab = []
 
+# Do kazdej z liter z pierwszego slowa, tworzymy tablice o wartosci 0.
+
 for i in range(stringLengthOne + 1):
     tab.append([0] * (stringLengthTwo + 1))
+
+# Sprawdzamy kazda litere w pierwszym slowie, jesli litera ze slowa pierwszego, jest taka sama, jak z slowa drugiego, maksymalny podciag rosnie o 1.
 
 for j in range(stringLengthOne):
     character = stringOne[j]
@@ -19,6 +26,8 @@ for j in range(stringLengthOne):
         tab[j+1][x+1] = max(tab[j+1][x], tab[j][x+1])
 
 maxN = -1000
+
+# Przyrownanie wartosci najdluzszego podciagu tych samych liter z obu wyrazow.
 
 for i in range(stringLengthOne):
     for j in range(stringLengthTwo):
