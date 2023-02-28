b3e021fd762e8eb5e2dbab80ed7249da82a66e14
Kuba7331 <kuba@kamionka.eu>
2023-02-28 13:24:10 +0100

    Comment update

--- palindromAlg.py
+++ palindromAlg.py
@@ -1,6 +1,9 @@
 string1 = input()
 newWord = string1.replace(" ", "")
 
+# Palindrom - slowo, ktore jest takie samo od tylu.
+# Algorytm sprawdza, czy slowo, od tylu, do srodka wyrazu, jest takie samo, jak ciag znakow od poczatku do srodka, jesli jest - to dane slowo jest palindromem.
+
 def isPalindrom(stringToCheck):
     middleOfString = len(stringToCheck) // 2
     for i in range(middleOfString):
