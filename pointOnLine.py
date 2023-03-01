c25a46b12ab82081648383ab6a4e3106918fa60a
Kuba7331 <kuba@kamionka.eu>
2023-03-01 20:17:18 +0100

    Comment update

--- pointOnLine.py
+++ pointOnLine.py
@@ -5,6 +5,8 @@ point2 = [2,4]
 
 point = [3,6]
 
+# Algorytm sprawdza, czy dany punkt nalezy do pewnej prostej/odcinka. Dziala to poprzez przyrownanie funkcji liniowej obu punktow polozonych na prostej, do wartosci sprawdzanego punktu.
+
 def ifOnLine(point1,point2,point):
     a = (point2[1] - point1[1]) / (point2[0] - point1[0])
     b = point2[1] - a*point2[0]
