5b8ad3bba63e9948c328e87fc363dfe4de3246e7
Kuba7331 <kuba@kamionka.eu>
2023-03-01 20:28:58 +0100

    Comment update

--- rest.py
+++ rest.py
@@ -1,6 +1,12 @@
-coinList = [1, 2, 5, 10, 50, 100, 200]
+coinList = [1,2, 5, 10, 20, 50, 100, 200, 500]
 price = int(input())
 coins = []
+
+# Algorytm wypisuje nam najlepsze ulozenie monet, aby uzyskac dana liczbe, z podanych nominalow z listy coinList.
+# Algorytm wykonuje sie do momentu, az roznica miedzy cena, a danym nominalem bedzie rowna 0.
+# Dzialamy od konca listy coinList, dodajemy kazdy element, ktory sprawil, ze dana liczba jest wieksza, lub rowna 0. Po czym od naszej ceny, odejmujemy liczbe z sprawdzanego nominalu i dalej przechodzimy przez petle.
+# Petla przerywa sie, w momencie, kiedy roznica miedzy cena a sprawdzanym nominalem jest mniejsza od 0, po czym sprawdza, czy dla mniejszych nominalow bedzie wieksza od 0.
+# Jesli jest, dodaje dany nominal do listy ulozenia monet do zaplaty.
 
 for i in range(len(coinList) -1, -1, -1):
     while price - coinList[i] >=0:
@@ -9,4 +15,3 @@ for i in range(len(coinList) -1, -1, -1):
 
 for j in coins:
     print(str(j)," ", end=None)
-
