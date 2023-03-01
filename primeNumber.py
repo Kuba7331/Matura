dea5d432899f6918f4ff717f251bd5dc6095e906
Kuba7331 <kuba@kamionka.eu>
2023-03-01 20:20:28 +0100

    Comment update

--- primeNumber.py
+++ primeNumber.py
@@ -2,6 +2,11 @@ import math
 
 isPrimeNumber = int(input())
 
+# Algorytm sprawdza, czy podana przez uzytkownika liczba, jest liczba pierwsza.
+# Liczba pierwsza, to liczba, ktora jest podzielna przez 1 i siebie.
+# Sprawdzamy, czy liczba jest pierwsza, metoda dzielenia z reszta, poczawszy od 2, do pierwiastka sprawdzanej liczby, poniewaz wiemy, ze nastepne liczby po tym pierwiastku, beda juz sprawdzone, bo to beda tylko i wylacznie ich wielokrotnosci.
+# Jesli przez caly proces dzielenia naszej liczby, przez wszystkie jej dzielniki, do jej pierwiastka, reszta bedzie rowna 0, to dana liczba nie jest liczba pierwsza.
+
 def isPrime(numberToCheck):
     if numberToCheck < 2 :
         return False
