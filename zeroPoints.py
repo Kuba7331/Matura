54699b978ce682c6400cae0e8e67960c0f232477
Kuba7331 <kuba@kamionka.eu>
2023-03-08 20:00:05 +0100

    Comment update

--- zeroPoints.py
+++ zeroPoints.py
@@ -4,8 +4,16 @@ b = interval[1]
 
 accuracyX = accuracyY = 0.05
 
+
+
 def functionY(x):
-    return (2 * x) + 4
+    return (2 * x) + 4 # -10 i 10 -100
+
+# Algorytm sprawdza, czy w danym przedziale, dla podanej funkcji istnieje miejsce zerowe.
+# Accuracy X i Y sluza do okreslenia granicy. Jesli dany wynik bedzie mniejszy od 0.05, to uznawane jest, ze nalezy do tego punktu.
+# Sprawdzenie, czy wystepuje dane miejsce zerowe nastepuja na zasadzie sprawdzenia, dla ktorego x, wartosc funkcji bedzie mniejsza od zera.
+# Z kazda iteracji petli zawezamy przedzial, do momentu, az ten przekroczy wartosc 0.05 dla wartosci Y. Wtedy oznacza to, ze znajduje sie tam miejsce zerowe.
+# Funkcja zwraca argument x, dla ktorego function(x) jest mniejsza od 0.05.
 
 def findingZeroPoints(begin, end, accuracyX, accuracyY):
     while abs(end - begin) >= accuracyX:
@@ -16,7 +24,7 @@ def findingZeroPoints(begin, end, accuracyX, accuracyY):
             end = middle
         else:
             begin = middle
-    return (begin + end) / 2
+    return round(middle,1)
 
 print(findingZeroPoints(a,b,accuracyX,accuracyY))
 
